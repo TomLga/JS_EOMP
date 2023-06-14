cc27e9be0949837132f3137eacf592ab27108faa
TomLga <semitallbox@gmail.com>
2023-06-14 14:46:30 +0200

    admin 1/2

--- scripts/admin.js
+++ scripts/admin.js
@@ -1,24 +1,24 @@
+//admin table
 function addItemsToTable() {
   let tableBody = document.getElementById("adminTableBody");
   clearTable(); // Clear existing table rows
   let products = JSON.parse(localStorage.getItem('products')) || [];
 
+  //creating rows inside the admin page 
   for (let i = 0; i < products.length; i++) {
-    let row = tableBody.insertRow(-1);
-    let cell1 = row.insertCell(0);
-    let cell2 = row.insertCell(1);
-    let cell3 = row.insertCell(2);
-    let cell4 = row.insertCell(3);
-    let cell5 = row.insertCell(4);
-    let cell6 = row.insertCell(5);
-
-    cell1.innerHTML = products[i].id;
-    cell2.innerHTML = products[i].name;
-    cell3.innerHTML = products[i].description;
-    cell4.innerHTML = products[i].price;
-
-    // Add Edit and Delete buttons if needed
-    // ...
+    let newRow = tableBody.insertRow(-1);
+    let block1 = newRow.insertCell(0);
+    let block2 = newRow.insertCell(1);
+    let block3 = newRow.insertCell(2);
+    let block4 = newRow.insertCell(3);
+    let block5 = newRow.insertCell(4); //edit btn
+    let block6 = newRow.insertCell(5); //del btn
+    
+    block1.innerHTML = products[i].id;
+    block2.innerHTML = products[i].name;
+    block3.innerHTML = products[i].description;
+    block4.innerHTML = products[i].price;
+    
   }
 }
 
@@ -30,6 +30,7 @@ function clearTable() {
 function addItemsToProductsContainer() {
   let productsContainer = document.getElementById("productsContainer");
   clearProductsContainer(); // Clear existing cards
+
   let products = JSON.parse(localStorage.getItem('products')) || [];
 
   for (let i = 0; i < products.length; i++) {
@@ -37,21 +38,41 @@ function addItemsToProductsContainer() {
 
     let card = document.createElement("div");
     card.classList.add("card");
-    card.style.width = "18rem";
+    card.style.width = "28rem";
 
     let cardBody = document.createElement("div");
     cardBody.classList.add("card-body");
 
-    cardBody.innerHTML =
-      "<h5 class='card-title'>" + product.name + "</h5>" +
-      "<p class='card-text'><strong>ID:</strong> " + product.id + "</p>" +
-      "<p class='card-text'><strong>Description:</strong> " + product.description + "</p>" +
-      "<p class='card-text'><strong>Price:</strong> $" + product.price + "</p>";
-
+    let shop =[{
+      id: id.value,
+      name:name.value,
+      description:description.value,
+      // img: img.value,
+      price: price.vaule,
+    }
+    ]
+   
+    //for each product to display on the cards not working 
+    let jungle = document.getElementById("productsContainer");
+shop.forEach((itemss) => {
+  jungle.innerHTML += `>
+       <td class= "card-body">${itemss.id}</td>
+       <td>${itemss.name}</td>
+      <td class="card-text">${itemss.description}</td>
+      // <td><img src="${itemss.img}"></td>
+       <td class="card-text">${itemss.price}></td>
+      <td><button>ADD</button></td>
+      <td><button>Delete</button></td>
+    `;
+});
     card.appendChild(cardBody);
     productsContainer.appendChild(card);
   }
 }
+
+
+
+
 
 function clearProductsContainer() {
   let productsContainer = document.getElementById("productsContainer");
@@ -96,6 +117,47 @@ window.onload = function() {
   addItemsToTable();
   addItemsToProductsContainer();
 };
+
+// let addNewItems=[
+//     {
+//         id:id,
+//         img:"",
+//         name:name,
+//         description:description,
+//         price:price,
+//     },
+// ]
+
+// let sweet = document.querySelector("#productsContainer");
+// addNewItems.forEach((thingg) => {
+//   sweet.innerHTML += `
+// <tr>
+//     <td>${thingg.name}</td>
+//     <td>${thingg.price}</td>
+//     <td><img src="${thingg.img}"></td>
+//     <td><button>ADD</button></td>
+//     <td><button>Delete</button></td>
+// </tr>`
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // to edit the product list 
